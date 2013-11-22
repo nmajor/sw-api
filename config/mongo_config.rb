de188a208d59eecd72af29c89b2a1f877ec777b0
Ethan Farmer <farmetha@gmail.com>
2013-11-22 16:21:34 -0700

    Fixed a misnamed variable

--- config/mongo_config.rb
+++ config/mongo_config.rb
@@ -5,7 +5,7 @@ class MongoConfig
     if ENV['MONGOHQ_URL']
       db = URI.parse(ENV['MONGOHQ_URL'])
       remote_name = db.path.gsub(/^\//, '')
-      MongoMapper.connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+      MongoMapper.connection = Mongo::Connection.new(db.host, db.port).db(remote_name)
       MongoMapper.connection.authenticate(db.user, db.password)
     else
       MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :pool_size => 5, :timeout => 5)
