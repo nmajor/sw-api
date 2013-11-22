3815dd613eafaf87cfb6064f9a524f4e1f87111b
Ethan Farmer <farmetha@gmail.com>
2013-11-22 11:10:06 -0700

    Stubbed out video urls

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello Startup Weeked Ogden"
 end
+
+get '/video' do
+  "TODO: List user videos"
+end
+
+get '/video/:id' do
+  "TODO: Get a specific video"
+end
+
+post '/video' do
+  "TODO: Create a video"
+end
