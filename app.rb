0244494925ba4b02b15e74ad3125e7db5beddc27
Ethan Farmer <farmetha@gmail.com>
2013-11-22 13:50:42 -0700

    Rough draft of the video and login api

--- app.rb
+++ app.rb
@@ -1,17 +1,58 @@
+require 'rubygems'
 require 'sinatra'
+require 'mongo_mapper';
+require 'json/ext'
+Dir[File.dirname(__FILE__) + "/models/*.rb"].each {|file| require file }
 
-get '/' do
-  "Hello Startup Weeked Ogden"
+configure do
+  MongoMapper.connection = Mongo::Connection.new('localhost',27017, :pool_size => 5, :timeout => 5)
+  MongoMapper.database = 'chronicle'
 end
 
-get '/video' do
-  "TODO: List user videos"
+enable :sessions
+
+# User methods
+post '/user' do
+  content_type :json
+  user = User.new({
+  	:username => params[:username],
+  	:password => params[:password]
+  	});
+  user.save
+  user.to_json
 end
 
-get '/video/:id' do
-  "TODO: Get a specific video"
+post '/login' do
+  user = User.first(:username => params[:username], :password => params[:password]);
+  unless user.nil?
+    session[:id] = user.id
+  end
+  user.id.to_json
+end
+
+get '/logout' do
+  session.clear
+end
+
+# Video methods
+get '/video/?' do
+  content_type :json
+  videos = Video.all(:user_id => session[:id])
+  videos.to_json
+end
+
+get '/video/:id/?' do
+  content_type :json
+  video = Video.find(params[:id], :user_id => session[:id])
+  video.to_json
 end
 
 post '/video' do
-  "TODO: Create a video"
+  video = Video.new({
+    :name => params[:name],
+    :description => params[:description],
+    :user_id => session[:id]
+  })
+  video.save
+  video.to_json
 end
--- models/video.rb
+++ models/video.rb
@@ -1,6 +1,7 @@
 class Video
   include MongoMapper::Document
 
+  key :user_id, ObjectId
   key :name, String
   key :description, String
   key :url, String
