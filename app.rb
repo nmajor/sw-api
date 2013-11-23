62edbe9663daa433afe3b8c46a75c26efc867d11
Nicholas Major <nic.major.d@gmail.com>
2013-11-23 15:12:37 -0700

    d

--- app.rb
+++ app.rb
@@ -48,7 +48,12 @@ get '/' do
     if session['access_token']
       graph = Koala::Facebook::GraphAPI.new(session["access_token"])
       current_fb_user = graph.get_object("me")
-      user = User.first( :fb_id => current_fb_user['id'].to_i )
+      user = User.new({
+      :fb_id => current_fb_user["id"],
+      :avatar => graph.get_picture("me", :type => "large"),
+      :name => current_fb_user["name"]
+      })
+      user.save
       "<h1>hey</h1> #{user.to_json} #{session['access_token'].to_s} #{current_fb_user} #{current_fb_user['id'].to_i}"
       #@current_user = get_current_user
       #erb :home
