627c892e0b8965149fe2006801fb68395d5bb110
Nicholas Major <nic.major.d@gmail.com>
2013-11-22 20:17:18 -0700

    I hope this actually fixes the mongo problems

--- config/mongo_config.rb
+++ config/mongo_config.rb
@@ -3,9 +3,7 @@ require 'mongo_mapper'
 class MongoConfig
   def self.configure
     if ENV['MONGOHQ_URL']
-      configure do
-          MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
-      end
+      MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
     else
       MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :pool_size => 5, :timeout => 5)
       MongoMapper.database = 'chronicle'
