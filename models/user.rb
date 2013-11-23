f042ce67e89c1d320c87b8ab47b4e24bc2dcddaf
Nicholas Major <nic.major.d@gmail.com>
2013-11-23 15:29:20 -0700

    d

--- models/user.rb
+++ models/user.rb
@@ -4,12 +4,12 @@ class User
   key :avatar, String
   key :name, String
   key :child_user_ids, Array, :typecast => 'ObjectId'
-#  many :child_users, :in => :child_user_ids, :class_name => 'User'
-  #many :video
+  many :child_users, :in => :child_user_ids, :class_name => 'User'
+  many :video
   timestamps!
 
-  #def parent_users
-  #  User.where(:child_user_ids => self.id)
-  #end
+  def parent_users
+    User.where(:child_user_ids => self.id)
+  end
 
 end
