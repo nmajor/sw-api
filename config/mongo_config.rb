2c62794b7846522509357677642366362d572bac
Nicholas Major <nic.major.d@gmail.com>
2013-11-22 20:19:44 -0700

    This better work

--- config/mongo_config.rb
+++ config/mongo_config.rb
@@ -3,7 +3,7 @@ require 'mongo_mapper'
 class MongoConfig
   def self.configure
     if ENV['MONGOHQ_URL']
-      MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
+      MongoMapper.setup({'production' => {'uri' => ENV['MONGOHQ_URL']}}, 'production')
     else
       MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :pool_size => 5, :timeout => 5)
       MongoMapper.database = 'chronicle'
