fdb0e3dbe40690fc1f9ae51621638a7f83448f31
Nicholas Major <nic.major.d@gmail.com>
2013-11-22 20:14:29 -0700

    I hope this fixes the mongo stuff

--- config/mongo_config.rb
+++ config/mongo_config.rb
@@ -3,10 +3,9 @@ require 'mongo_mapper'
 class MongoConfig
   def self.configure
     if ENV['MONGOHQ_URL']
-      db = URI.parse(ENV['MONGOHQ_URL'])
-      remote_name = db.path.gsub(/^\//, '')
-      MongoMapper.connection = Mongo::Connection.new(db.host, db.port).db(remote_name)
-      MongoMapper.connection.authenticate(db.user, db.password)
+      configure do
+          MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
+      end
     else
       MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :pool_size => 5, :timeout => 5)
       MongoMapper.database = 'chronicle'
